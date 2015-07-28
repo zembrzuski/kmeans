4a3cc46de39a8d9f0c550ed4994c3626cc9a5b25
Your Name <you@example.com>
2015-07-28 15:17:44 -0300

    it's pumping

--- usingkmeans.R
+++ usingkmeans.R
@@ -1,5 +1,11 @@
 rm(list = ls())
 
+setwd('/home/nozes/Desktop/studying/data_science_getting_guru/k-means-project/github/kmeans')
+source('clustering.R')
+source('graphics.R')
+source('kmeans.R')
+source('centroids.R')
+
 data <- matrix(runif(100, 0, 10), ncol = 2)
 colnames(data) <- c('x', 'y')
 plot(data)
